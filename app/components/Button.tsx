cf049023d507b59025a99f874e9767f845308435
Ayushjodd <ayushkalonia2016@gmail.com>
2024-08-04 18:14:54 +0530

    secondary button for google

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -10,7 +10,7 @@ function PrimaryButton({
   return (
     <button
       onClick={onClick}
-      className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium orunded-lg text-sm px-5 py-2.5 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-2xl text-sm px-5 py-2.5 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
     >
       {children}
     </button>
@@ -18,3 +18,24 @@ function PrimaryButton({
 }
 
 export default PrimaryButton;
+
+export const GoogleButton = ({
+  children,
+  onClick,
+  prefix,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+  prefix?: React.ReactNode;
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      className="  text-white bg-blue-500 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-6 py-3 me-2 mb-2 flex"
+    >
+      <div>{prefix}</div>
+      <div>{children} </div>
+    </button>
+  );
+};
